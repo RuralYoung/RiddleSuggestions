f9131168e49db2eb2b0e6d037b59186c345de1bb
RuralYoung <ruralyoungiii@gmail.com>
2023-06-29 23:16:37 -0400

    Add project files.

---
+++ Models/RiddleSuggestion.cs
@@ -0,0 +1,11 @@
+﻿namespace RiddleSuggestions.Models
+{
+    public class RiddleSuggestion
+    {
+        public int RiddleID { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string? Hint { get; set; }
+        public int Rating { get; set; }
+    }
+}
