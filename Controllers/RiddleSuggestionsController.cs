dafaa3ba708a537b004510c7149f8206700df5da
RuralYoung <ruralyoungiii@gmail.com>
2023-07-02 18:00:48 -0400

    Created a RiddleSuggestions Context
    Also added a controller

---
+++ Controllers/RiddleSuggestionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RiddleSuggestions.Controllers
+{
+    public class RiddleSuggestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using RiddleSuggestions.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddDbContext<RiddleSuggestionsDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration[
+        "ConnectionStrings:DefaultConnectionString"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+
+builder.Services.AddControllers();
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapControllers();
 
 app.Run();
