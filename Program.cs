102a359cf3e8e649d862c2a87b9997f26f3e9bbd
RuralYoung <ruralyoungiii@gmail.com>
2023-07-05 21:41:24 -0400

    Fixed CORS issue

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using RiddleSuggestions.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddDbContext<RiddleSuggestionsDbContext>(opts =>
 {
     opts.UseSqlServer(builder.Configuration[
@@ -11,13 +10,23 @@ builder.Services.AddDbContext<RiddleSuggestionsDbContext>(opts =>
     opts.EnableSensitiveDataLogging(true);
 });
 
-builder.Services.AddControllers();
+builder.Services.AddCors( opt =>
+{
+    opt.AddPolicy("AllowAnyOrigin", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
-builder.Services.AddCors();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.MapControllers();
+
+app.UseCors("AllowAnyOrigin");
 
 app.MapGet("/", () => "Hello World!");
 
